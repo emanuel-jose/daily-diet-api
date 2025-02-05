396e4c92662b7de6ee4ccbb39a317e031c7d0ee7
emanuel-jose <emanuel.gotstark@gmail.com>
2025-02-05 20:15:00 -0300

    feat: fix error request cases

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,39 +7,56 @@ import bcrypt from "bcrypt";
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/", async (request, reply) => {
     const createUserBodySchema = z.object({
-      name: z.string(),
-      email: z.string().email(),
-      password: z.string(),
+      name: z.string().min(1, "name not be empty"),
+      email: z.string().email("invalid email"),
+      password: z
+        .string()
+        .min(3, "password must be equal or longer than 3 characters"),
     });
 
-    const sessionId = randomUUID();
-    const userId = randomUUID();
+    try {
+      const { name, email, password } = createUserBodySchema.parse(
+        request.body
+      );
 
-    const { name, email, password } = createUserBodySchema.parse(request.body);
+      const emailAlreadyExist = await knex("users")
+        .where("email", email)
+        .first();
 
-    const emailAlreadyExist = await knex("users").where("email", email).first();
+      if (emailAlreadyExist) {
+        return reply.status(400).send({ message: "email already exist!" });
+      }
 
-    if (emailAlreadyExist) {
-      return reply.status(400).send({ message: "email already exist!" });
+      const sessionId = randomUUID();
+      const userId = randomUUID();
+
+      const token = app.jwt.sign({ userId });
+      const hashPassword = await bcrypt.hash(password, 10);
+
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7,
+      });
+      reply.cookie("token", token);
+
+      await knex("users").insert({
+        id: userId,
+        session_id: sessionId,
+        name,
+        email,
+        password: hashPassword,
+      });
+
+      return reply.status(201).send({ token });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const formattedErrors = error.issues.map((issue) => ({
+          path: issue.path.join("."),
+          message: issue.message,
+        }));
+
+        reply.status(400).send({ errors: formattedErrors });
+      }
     }
-
-    const token = app.jwt.sign({ userId });
-    const hashPassword = await bcrypt.hash(password, 10);
-
-    reply.cookie("sessionId", sessionId, {
-      path: "/",
-      maxAge: 60 * 60 * 24 * 7,
-    });
-    reply.cookie("token", token);
-
-    await knex("users").insert({
-      id: userId,
-      session_id: sessionId,
-      name,
-      email,
-      password: hashPassword,
-    });
-
-    return reply.status(201).send({ token });
   });
 }
