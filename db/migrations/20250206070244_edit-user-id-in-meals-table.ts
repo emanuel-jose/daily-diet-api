714c2d97c0eea14b78fae0bbc895a6d5d8e1218d
emanuel-jose <emanuel.gotstark@gmail.com>
2025-02-06 04:10:53 -0300

    fix: set id reference in migration

---
+++ db/migrations/20250206070244_edit-user-id-in-meals-table.ts
@@ -0,0 +1,15 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("meals", (table) => {
+    table.dropColumn("user_id");
+    table.uuid("user_id").references("users.id").notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("meals", (table) => {
+    table.dropColumn("user_id");
+    table.uuid("user_id");
+  });
+}
